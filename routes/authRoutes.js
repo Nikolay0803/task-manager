f0366d00b42f0018dec6d6c9c0c1f0ff8a131eb8
Nikolay0803 <battmandu@gmail.com>
2024-01-24 15:12:11 +0200

    commm

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import * as authController from '../controllers/authControler.js';
+import * as authController from "../controllers/authControler.js";
 
 const router = express.Router();
 
-router.post('/auth/register', authController.register)
-router.post('/auth/login', authController.login)
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
 
 export default router;
